469fbdf72ca1da9056c9c6b2bfe5c76e9a4af4f7
Brunno Sena <brunnow@hotmail.com>
2021-10-19 23:40:46 -0300

    🇮🇱  Ajuste cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
@@ -6,6 +7,8 @@ import { Server } from 'socket.io';
 import { router } from './routes';
 
 const app = express();
+app.use(cors());
+
 const serverHttp = http.createServer(app);
 
 const io = new Server(serverHttp, {
@@ -15,26 +18,23 @@ const io = new Server(serverHttp, {
 });
 
 io.on('connection', (socket) => {
-  console.log(`User connected in socket ${socket.id}`);
-
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
+  console.log(`Usuário conectado no socket ${socket.id}`);
 });
 
 app.use(express.json());
+
 app.use(router);
 
-app.get('/github', (req, res) => {
-  res.redirect(
+app.get('/github', (request, response) => {
+  response.redirect(
     `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
   );
 });
 
-app.get('/signin/callback', (req, res) => {
-  const { code } = req.query;
+app.get('/signin/callback', (request, response) => {
+  const { code } = request.query;
 
-  return res.json(code);
+  return response.json(code);
 });
 
 export { serverHttp, io };
